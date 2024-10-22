0a4904f1bdb9f0d7471071793fb3ee67c04496e2
ShakirMahmud <shakirmahmud50@gmail.com>
2024-10-22 16:19:16 +0600

    display blog data and hashtags

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,26 @@
 import PropTypes from "prop-types";
 const Blog = ({blog}) => {
     console.log(blog)
+    const {title, cover, reading_time, author_img, author, posted_date, hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`Cover picture of ${title}`} />
+            <div className="flex justify-between">
+                <div className="flex gap-6 items-center justify-center">
+                    <img className="w-14 rounded-full" src={author_img} alt="" />
+                    <div className="">
+                        <h3 className="text-2xl">{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            {
+                hashtags.map((hash, idx)=><span key={idx}><a href="">{hash}</a></span>)
+            }
         </div>
     );
 };
